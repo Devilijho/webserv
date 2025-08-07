a4f0c703a8c6e01c414c153267562447dee4e565
Santiago Fuentes <santi.fuentes.llanos@gmail.com>
2025-08-07 14:26:52 +0200

    Working on static files

--- main.cpp
+++ main.cpp
@@ -1,34 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pde-vara <pde-vara@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/06 12:01:59 by pde-vara          #+#    #+#             */
-/*   Updated: 2025/08/06 15:12:52 by pde-vara         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "CGI/CGIHandler.hpp"
 #include "server.hpp"
 
-// int	main(void)
-// {
-// 	int returnValue = htpp_request();
-// 	std::cout << "REQUEST RETURN VALUE : " << returnValue << std::endl;
-// 	return (0);
-// }
 
-int main()
+int	main(void)
 {
-    // 1. Create a server instance listening on port 8080
-    Server server(8080);
-
-    // 2. Start the server
-    if (!server.start()) {
-        return 1; // Something went wrong
-    }
-
-    return 0;
+	int returnValue = htpp_request();
+	if (returnValue == 0)
+		returnValue = OK;
+	std::cout << "REQUEST RETURN VALUE : " << returnValue << std::endl;
+	return (0);
 }
+
+// int main()
+// {
+//     // 1. Create a server instance listening on port 8080
+//     Server server(8080);
+
+//     // 2. Start the server
+//     if (!server.start()) {
+//         return 1; // Something went wrong
+//     }
+
+//     return 0;
+// }
