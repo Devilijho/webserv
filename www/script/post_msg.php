62484c813861c6d3ce62081584e9cbb03ffbb3a4
Valeria Flores <vflores@f6r13s12.clusters.42paris.fr>
2025-08-21 15:07:42 +0200

    PATH INFO de linux

--- www/script/post_msg.php
+++ www/script/post_msg.php
@@ -1,6 +1,6 @@
 <?php
 // echo "<br/>CONTENT_TYPE: " . $_SERVER["CONTENT_TYPE"];
-// echo "<br/>LENGTH: " . $_SERVER["CONTENT_LENGTH"];
+echo "<br/>LENGTH: " . $_SERVER["CONTENT_LENGTH"];
 // echo "SCRIPT_NAME: " . $_SERVER["SCRIPT_FILENAME"];
 
 echo "<br/>POST msg:" . $_POST["msg"];
